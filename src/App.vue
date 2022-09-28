a6c53e76d26a5b07d6984515c1e794420cf9fd16
mariyamateo <ma.clarissepmateo@gmail.com>
2022-09-28 19:52:14 +0800

    on delete item

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
+    <AddTodo v-on:add-todo="addTodo" />
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
 import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
 
 export default {
   name: "App",
   components: {
     Todos,
+    AddTodo,
   },
   data() {
     return {
@@ -37,6 +40,9 @@ export default {
     deleteTodo(id) {
       this.todos = this.todos.filter((todo) => todo.id !== id);
     },
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
+    },
   },
 };
 </script>
@@ -50,5 +56,16 @@ export default {
 
 body {
   font-family: Arial, Helvetica, sans-serif;
+}
+.btn {
+  display: inline;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #666;
 }
 </style>
